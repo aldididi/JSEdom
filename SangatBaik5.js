22928444b479c561e75c545a41d0c3bb24f67417
Aldy Heryana <32344944+aldididi@users.noreply.github.com>
2020-06-26 23:04:39 +0700

    Create SangatBaik5.js

---
+++ SangatBaik5.js
@@ -0,0 +1,12 @@
+//Sangat Kurang = 1
+//Kurang = 2
+//Cukup = 3
+//Baik = 4
+//Sangat Baik = 5
+var autoedom = document.querySelectorAll('input[type="radio"]');
+var score = 5;
+for (var i = 0; i < autoedom.length; i++) {
+    if (autoedom[i].value == score) {
+        autoedom[i].checked = true;
+    }
+}
